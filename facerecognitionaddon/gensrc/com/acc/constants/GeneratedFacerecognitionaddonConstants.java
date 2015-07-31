fd457865fd7ea31aa3b2c7f2e6cf6c74b4c29837
swapniilpandey <swapnil.a.pandey@accenture.com>
2015-07-31 12:00:08 +0530

    FaceRecognitionAddon

---
+++ facerecognitionaddon/gensrc/com/acc/constants/GeneratedFacerecognitionaddonConstants.java
@@ -0,0 +1,28 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 29, 2015 3:01:35 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedFacerecognitionaddonConstants
+{
+	public static final String EXTENSIONNAME = "facerecognitionaddon";
+	public static class TC
+	{
+		public static final String FACERECOGNITIONCOMPONENT = "FaceRecognitionComponent".intern();
+	}
+	
+	protected GeneratedFacerecognitionaddonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ facerecognitionaddon/src/com/acc/constants/FacerecognitionaddonConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package com.acc.constants;
+
+/**
+ * Global class for all Facerecognitionaddon constants. You can add global constants for your extension into this class.
+ */
+public final class FacerecognitionaddonConstants extends GeneratedFacerecognitionaddonConstants
+{
+	public static final String EXTENSIONNAME = "facerecognitionaddon";
+
+	private FacerecognitionaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
