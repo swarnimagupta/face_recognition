6db6ff0b05519ba39a6565a5135e77f00e0942c0
swarnimagupta <swarniima@gmail.com>
2015-08-19 11:24:14 +0530

    saving customer image fix

--- facerecognitionaddon/src/com/acc/util/HelperUtil.java
+++ facerecognitionaddon/src/com/acc/util/HelperUtil.java
@@ -4,10 +4,10 @@
 package com.acc.util;
 
 import java.io.File;
+import java.util.Date;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
 
 
 /**
@@ -17,9 +17,9 @@ import org.joda.time.DateTime;
 public class HelperUtil
 {
 
-	private static final String SEPARATOR = "\\";
-	private static final String HYBRIS_DATA = "\\hybris\\data\\";
-	private static final String IMAGE2 = "\\image\\";
+	private static final String SEPARATOR = "/";
+	private static final String HYBRIS_DATA = "/hybris/data";
+	private static final String IMAGE2 = "/image";
 	private static final Logger LOG = Logger.getLogger(HelperUtil.class);
 
 	/**
@@ -28,12 +28,25 @@ public class HelperUtil
 	public static String createDirectory(final String uid)
 	{
 		StringBuilder location = new StringBuilder("C:" + HYBRIS_DATA);
-		final String dateTime = new DateTime().toString("dd-MM-yy HH:mm:ss");
+		final String dateTime = (new Date()).toString();
 		location.append(IMAGE2);
+		final String output = dateTime.toString().replace(":", "-");
+		location.append(SEPARATOR).append(output);
+		location.append(SEPARATOR).append(uid);
 		File g = new File(location.toString());
 		if (!g.exists())
 		{
-			if (g.mkdir())
+			boolean result = false;
+			try
+			{
+				g.mkdirs();
+				result = true;
+			}
+			catch (final SecurityException se)
+			{
+				LOG.error("could not create directory", se);
+			}
+			if (result)
 			{
 				LOG.info("image Directory is created!" + dateTime.toString());
 			}
@@ -43,10 +56,22 @@ public class HelperUtil
 				//creating for linux or mac
 				location = new StringBuilder(HYBRIS_DATA);
 				location.append(IMAGE2);
+				location.append(SEPARATOR).append(output);
+				location.append(SEPARATOR).append(uid);
 				g = new File(location.toString());
 				if (!g.exists())
 				{
-					if (g.mkdir())
+					result = false;
+					try
+					{
+						g.mkdirs();
+						result = true;
+					}
+					catch (final SecurityException se)
+					{
+						LOG.error("could not create directory", se);
+					}
+					if (result)
 					{
 						LOG.info("image Directory is created!" + dateTime.toString());
 					}
@@ -57,22 +82,21 @@ public class HelperUtil
 				}
 			}
 		}
-		else
-		{
-			final String output = dateTime.toString().replace(":", "-");
-			location.append(SEPARATOR).append(output);
-			final File f = new File(g + SEPARATOR + output);
-			LOG.info("datetime Directory is created!" + output + g.toString());
-			f.mkdir();
-			LOG.info("datetime Directory is created after f!" + f.toString());
-			if (f.exists())
-			{
-				location.append(SEPARATOR).append(uid);
-				final File e = new File(f + SEPARATOR + uid);
-				e.mkdir();
-				LOG.info("customerID folder is created after f!" + f.toString());
-			}
-		}
+		//		else
+		//		{
+		//			//location.append(SEPARATOR).append(output);
+		//			final File f = new File(g);
+		//			LOG.info("datetime Directory is created!" + output + g.toString());
+		//			f.mkdir();
+		//			LOG.info("datetime Directory is created after f!" + f.toString());
+		//			if (f.exists())
+		//			{
+		//				location.append(SEPARATOR).append(uid);
+		//				final File e = new File(f + SEPARATOR + uid);
+		//				e.mkdir();
+		//				LOG.info("customerID folder is created after f!" + f.toString());
+		//			}
+		//		}
 		return location.toString();
 	}
 
